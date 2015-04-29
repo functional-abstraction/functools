3ae21c6045c40744e7bab8ed71f9ad1b963fbcdf
aureooms <aurelien.ooms@gmail.com>
2015-04-29 10:23:48 +0200

    support AMD loaders

--- js/dist/functools.js
+++ js/dist/functools.js
@@ -1,6 +1,8 @@
-(function(exports, undefined){
+( function ( ) {
 
-	'use strict';
+'use strict' ;
+
+var definition = function ( exports , undefined ) {
 
 
 /* js/src/apply.js */
@@ -264,4 +266,16 @@ var starcompose = function ( callables ) {
 
 exports.starcompose = starcompose ;
 
-})(typeof exports === 'undefined' ? this['functools'] = {} : exports);
+return exports ;
+} ;
+if ( typeof exports === "object" ) {
+	definition( exports ) ;
+}
+else if ( typeof define === "function" && define.amd ) {
+	define( "aureooms-js-functools" , [ ] , function ( ) { return definition( { } ) ; } ) ;
+}
+else if ( typeof window === "object" && typeof window.document === "object" ) {
+	definition( window["functools"] = { } ) ;
+}
+else console.error( "unable to detect type of module to define for aureooms-js-functools") ;
+} )( ) ;
